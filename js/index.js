eb378b2ae22c82bc826ce6adaa2cec7d54a7e15d
daviddao30 <daivudao30@gmail.com>
2024-06-12 22:08:39 -0400

    EZPark Landing Page

---
+++ js/index.js
@@ -0,0 +1,6 @@
+console.log("Your index.js file is loaded correctly!");
+
+function myFunction() {
+    const element = document.getElementById("cta");
+    element.scrollIntoView({block: "end", behavior: "smooth"});
+  }
